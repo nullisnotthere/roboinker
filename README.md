9e0613d779e1421cae31b72bf84b22c39819cc37
nullisnotthere <lukedefault@protonmail.com>
2025-02-12 11:12:41 +1100

    Added README

---
+++ README.md
@@ -0,0 +1,1 @@
+# AI-powered 2-DOF Robotic Arm that draws what you ask it to
--- firmware/test/test.ino
+++
@@ -1,8 +0,0 @@
-void setup() {
-  Serial.begin(9600);
-  Serial.println("Started.");
-}
-
-void loop() {
-  Serial.println("loop test");
-}
